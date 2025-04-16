6014a1fbe0b835d8f866599016ef94af4fa3a0bc
최예진 <cyjbbh@gmail.com>
2025-04-16 13:43:34 +0900

    [COMMIT][SETTING][server down 오류 잡음]

--- src/main/java/com/learn/bbs/pltad/dao/impl/PltadDaoImpl.java
+++ src/main/java/com/learn/bbs/pltad/dao/impl/PltadDaoImpl.java
@@ -3,9 +3,11 @@ package com.learn.bbs.pltad.dao.impl;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.learn.bbs.pltad.dao.PltadDao;
 
+@Repository
 public class PltadDaoImpl extends SqlSessionDaoSupport implements PltadDao {
 	@Autowired
     @Override
