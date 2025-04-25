13330563e39bf1eb4e5d392ccae1a6445eeefbb2
Tony Kang <lgtwins0302@gmail.com>
2025-04-25 13:03:47 +0900

    [COMMIT][BUG FIX][강좌의 정원이 차거나 빠졌을 시 대응하는 기능 추가]

--- src/main/java/com/learn/bbs/crs/apphstr/web/AppHstrController.java
+++ src/main/java/com/learn/bbs/crs/apphstr/web/AppHstrController.java
@@ -2,12 +2,11 @@ package com.learn.bbs.crs.apphstr.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.learn.bbs.crs.apphstr.service.AppHstrService;
-import com.learn.bbs.crs.apphstr.vo.AppHstrVO;
 import com.learn.bbs.usr.vo.UsrVO;
 import com.learn.exceptions.AccessDeniedException;
 
@@ -24,10 +23,10 @@ public class AppHstrController {
     @Autowired
     private AppHstrService appHstrService;
     
+    @ResponseBody
     @PostMapping("/insttn/usr/detail/{crsInfId}/register")
     public String doRegisterCourse(@PathVariable String crsInfId,
-                                    Model model,
-                                    HttpSession session) {
+                                   HttpSession session) {
         UsrVO usrVO = (UsrVO) session.getAttribute("__LOGIN_USER__");
         
         if (usrVO == null) {
@@ -36,19 +35,12 @@ public class AppHstrController {
 
         String usrMl = usrVO.getUsrMl();
         
-        // '강좌 신청 시' 정원 초과 체크
-        boolean isCourseFull = this.appHstrService.insertOneAppHstr(crsInfId, usrMl);
+        boolean isSuccess = this.appHstrService.insertOneAppHstr(crsInfId, usrMl);
 
-        // 정원 초과일 경우
-        if (!isCourseFull) {
-            model.addAttribute("isCourseFull", true);
-            return "insttn/usr/detail/" + crsInfId;
+        if (!isSuccess) {
+            return "FULL"; // 정원 초과
         }
 
-        // 정원 초과 아닐 경우
-        this.appHstrService.insertOneAppHstr(crsInfId, usrMl);
-        
-        model.addAttribute("isCourseFull", false);
-        return "insttn/usr/detail/" + crsInfId;
+        return "OK"; // 신청 완료
     }
 }
